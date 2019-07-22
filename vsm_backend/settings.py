8635f9ab06c68d5b6e7c1829df7f2949b1fbeea4
Sahil333 <sahilvs000@gmail.com>
2019-07-22 12:59:42 +0530

    Update .travis.yml for production build

--- vsm_backend/settings.py
+++ vsm_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',
                             )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=1))
+DEBUG = bool(int(os.environ.get("DEBUG", default=1)))
 
 ALLOWED_HOSTS = ['*']
 
